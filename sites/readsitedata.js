f6f5aff2f4ddb7083a95c7648a73940f99dd8abf
jim traugott <jtraug@gmail.com>
2018-11-03 17:31:30 -0700

    Fixed some quotes

--- sites/readsitedata.js
+++ sites/readsitedata.js
@@ -17,7 +17,7 @@ var parseAndRead = function(str) {
 ///    console.log(siteData.value.timeSeries[0]);
     siteData.value.timeSeries.forEach (function(element, index, array) {
         if (element.variable.variableName === "Gage height, ft") {
-            console.log("{'siteIndex' : '" + element.sourceInfo.siteCode[0].value + "', 'siteName' : '" + element.sourceInfo.siteName + "'},");
+            console.log("{\"siteIndex\": \"" + element.sourceInfo.siteCode[0].value + "\", \"siteName\" : \"" + element.sourceInfo.siteName + "\"},");
         }
 //        console.log(element.sourceInfo.siteName);
 //        console.log(element.sourceInfo.siteCode[0].value);
